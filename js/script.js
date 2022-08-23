08d4e186aaf3474ea06aa8e65208931da44cbad7
galaraz <jvictor.si.2013@gmail.com>
2022-08-23 00:32:11 -0300

    formatCurrency and toUpperCase

--- js/script.js
+++ js/script.js
@@ -205,6 +205,26 @@ verifyName(token) {
     }
 }
 
+
+  formatCurrency() {
+    var element = document.getElementById('balance-input');
+    var value = element.value;
+
+    value = value + '';
+    value = parseInt(value.replace(/[\D]+/g, ''));
+    value = value + '';
+    value = value.replace(/([0-9]{2})$/g, ",$1");
+
+    if (value.length > 6) {
+        value = value.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    }
+
+    element.value = value;
+    if(value == 'NaN') element.value = '';
+  }
+
+
+
 }
 
 let tokensController = new TokensController()
